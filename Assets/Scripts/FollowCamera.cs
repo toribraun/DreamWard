46211c509cce1e1e62a81520f42951b72631ba29
toribraun <torribraun161@gmail.com>
2021-09-29 18:00:53 +0500

    Init Unity Project

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] 
+    private float speed = 7F;
+    [SerializeField] 
+    private Transform player;
+
+    void Awake()
+    {
+        if (!player)
+            player = FindObjectOfType<Unit>().transform;
+
+    }
+
+    void Update ()
+    {
+        var position = player.position;
+        position.z = -10F;
+        
+        if (player.position.y < 5)
+            position.y = 0;
+        position.y *= 0.9F;
+            
+        transform.position = Vector3.Lerp(transform.position, position, speed * Time.deltaTime);
+    }
+}
