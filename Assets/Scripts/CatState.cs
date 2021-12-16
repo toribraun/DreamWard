c054b128ea43b32e7b98a414a23853c48ae32e27
toribraun <torribraun161@gmail.com>
2021-12-16 19:09:35 +0500

    Level2. music + maze

--- Assets/Scripts/CatState.cs
+++ Assets/Scripts/CatState.cs
@@ -1,4 +1,4 @@
-﻿public enum CatState
+public enum CatState
 { 
     Idle = 0,
     Run = 1,
--- Assets/Scripts/Firefly.cs
+++ Assets/Scripts/Firefly.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Firefly : Unit
 {
     private Cat cat;
-    
+
     private enum State
     {
         Free,
