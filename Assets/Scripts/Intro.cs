ad780435e1a1d8dc4c502333d6e39c645ef536f9
ColourBright <katerinka_post@mail.ru>
2022-01-24 17:45:54 +0500

    intro, some ui screens settings

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour
+{
+    [SerializeField]
+    private float time = 15F;
+    [SerializeField]
+    private string nextScene = "MainMenu";
+
+    public void Start()
+    {
+    }
+
+    public void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
